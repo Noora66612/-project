037586b7295a1e4f682354fb9e2cf8513db79add
norashh <norashih04612@gmail.com>
2024-05-19 00:33:30 +0800

    首頁1.0
    首頁

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("menu-icon").addEventListener("click", function() {
+    document.getElementById("mySidebar").classList.toggle("active");
+});
+
+document.getElementById("close-btn").addEventListener("click", function() {
+    document.getElementById("mySidebar").classList.toggle("active");
+});
+
+document.addEventListener("click", function(event) {
+    var sidebar = document.getElementById("mySidebar");
+    var menuIcon = document.getElementById("menu-icon");
+
+    // Check if the click is outside the sidebar and the menu icon
+    if (!sidebar.contains(event.target) && !menuIcon.contains(event.target)) {
+        sidebar.classList.remove("active");
+    }
+});
+
+document.addEventListener("scroll", function(event) {
+    var sidebar = document.getElementById("mySidebar");
+    var menuIcon = document.getElementById("menu-icon");
+
+    // Check if the click is outside the sidebar and the menu icon
+    if (!sidebar.contains(event.target) && !menuIcon.contains(event.target)) {
+        sidebar.classList.remove("active");
+    }
+});
+
+var btns = document.querySelectorAll(".btn1");
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        var computedStyle = window.getComputedStyle(btn);
+        var color = computedStyle.getPropertyValue("color");
+        
+        if (color === "rgb(214, 76, 76)") {
+            btn.style.color = "#524d4d";
+        } else {
+            btn.style.color = "#D64C4C";
+        }
+    });
+});
+
+
