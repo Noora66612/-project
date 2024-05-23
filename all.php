e4d5afb811c824f33a3cb71d80c182e6106638d9
Ya93722 <meldy.email@gmail.com>
2024-05-23 13:55:14 +0800

    添加了新功能

--- all.php
+++ all.php
@@ -27,47 +27,54 @@
     
 </head>
 <body>
-    <!--- header --->
-    <header>
-        <a href="home.php" class="logo">央央熊食在</a>
+     <!--- header --->
+     <header>
+        <a href="#" class="logo">央央熊食在</a>
         <ul class="navlist">
             <li class="nav-item">
-                <a href="home.php" class="nav-link">首頁</a>
+                <a href=home.php class="nav-link">首頁</a>
             </li>
             <li class="nav-item">
-                <a href="all.php" class="nav-link">所有餐廳</a>
+                <a href=all.php class="nav-link">所有餐廳</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">用餐地點</a>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  用餐地點
+                </a>
                 <ul class="dropdown-menu">
-                    <li><a href="school.php" class="dropdown-item">學餐</a></li>
-                    <li><a href="backdoor.php" class="dropdown-item">後門</a></li>
-                    <li><a href="street.php" class="dropdown-item">宵夜街</a></li>
+                  <li><a href=school.php class="dropdown-item">學餐</a></li>
+                  <li><a href=backdoor.php class="dropdown-item">後門</a></li>
+                  <li><a href=street.php class="dropdown-item">宵夜街</a></li>
                 </ul>
             </li>
         </ul> 
+
         <div class="nav-right d-flex align-items-center">
             <form class="d-flex align-items-center search-form" style="margin-right: 15px;">
-                <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
-                <button class="search-btn" type="button">
-                    <i class="ri-search-line"></i>
-                </button>
+                <form class="d-flex align-items-center search-form" style="margin-right: 15px;">
+                    <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
+                    <button class="search-btn" type="button">
+                        <i class="ri-search-line"></i>
+                    </button>
+                </form> 
             </form>            
-            <a href="#"><i class="ri-heart-line"></i></a>
-            <a href="#"><i class="ri-user-line"></i></a>
+            <a href="myfavfix.html"><i class="ri-heart-line"></i></a>
+            <a href="user.html"><i class="ri-user-line"></i></a>
             <div class="bx bx-menu" id="menu-icon"></div>
         </div>  
     </header>
+
     <!--- sidebar --->
     <div id="mySidebar" class="sidebar">
         <a href="javascript:void(0)" class="close-btn" id="close-btn">&times;</a>
         <a href="home.php">首頁</a>
-        <a href="all.php">所有餐廳</a>
-        <a href="school.php">學餐</a>
+        <a href=all.php>所有餐廳</a>
+        <a href=school.php>學餐</a>
         <a href="backdoor.php">後門</a>
         <a href="street.php">宵夜街</a>
         <a href="#">聯絡我們</a>
     </div>
+
 
     <!--- 所有餐廳 --->
     <section class="n-restaurants">
--- street.php
+++ street.php
@@ -4,61 +4,78 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>央央熊食在</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+    crossorigin="anonymous"></script>
+    <title>央央熊食在</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+
+    <!-- box-icon link -->
+    <link rel="stylesheet" 
+     href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <!-- remix-icons link -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+    rel="stylesheet"/>
+    
+    <!-- google fonts link -->
+
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    
 </head>
 <body>
 
-<header>
-    <a href="home.html" class="logo">央央熊食在</a>
-    <ul class="navlist">
-        <li class="nav-item">
-            <a href=home.html class="nav-link">首頁</a>
-        </li>
-        <li class="nav-item">
-            <a href=all.html class="nav-link">所有餐廳</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              用餐地點
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href=school.html class="dropdown-item">學餐</a></li>
-              <li><a href=backdoor.html class="dropdown-item">後門</a></li>
-              <li><a href=street.html class="dropdown-item">宵夜街</a></li>
-            </ul>
-        </li>
-    </ul> 
+ <!--- header --->
+ <header>
+        <a href="#" class="logo">央央熊食在</a>
+        <ul class="navlist">
+            <li class="nav-item">
+                <a href=home.php class="nav-link">首頁</a>
+            </li>
+            <li class="nav-item">
+                <a href=all.php class="nav-link">所有餐廳</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  用餐地點
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href=school.php class="dropdown-item">學餐</a></li>
+                  <li><a href=backdoor.php class="dropdown-item">後門</a></li>
+                  <li><a href=street.php class="dropdown-item">宵夜街</a></li>
+                </ul>
+            </li>
+        </ul> 
 
-    <div class="nav-right d-flex align-items-center">
-        <form class="d-flex align-items-center search-form" style="margin-right: 15px;">
+        <div class="nav-right d-flex align-items-center">
             <form class="d-flex align-items-center search-form" style="margin-right: 15px;">
-                <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
-                <button class="search-btn" type="button">
-                    <i class="ri-search-line"></i>
-                </button>
-            </form> 
-        </form>            
-        <a href="#"><i class="ri-heart-line"></i></a>
-        <a href="#"><i class="ri-user-line"></i></a>
-        <div class="bx bx-menu" id="menu-icon"></div>
-    </div>  
-</header>
+                <form class="d-flex align-items-center search-form" style="margin-right: 15px;">
+                    <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
+                    <button class="search-btn" type="button">
+                        <i class="ri-search-line"></i>
+                    </button>
+                </form> 
+            </form>            
+            <a href="myfavfix.html"><i class="ri-heart-line"></i></a>
+            <a href="user.html"><i class="ri-user-line"></i></a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>  
+    </header>
 
-<div id="mySidebar" class="sidebar">
-    <a href="javascript:void(0)" class="close-btn" id="close-btn">&times;</a>
-    <a href="home.html">首頁</a>
-    <a href=all.html>所有餐廳</a>
-    <a href=school.html>學餐</a>
-    <a href="backdoor.html">後門</a>
-    <a href="street.html">宵夜街</a>
-    <a href="#">聯絡我們</a>
-</div>
+    <!--- sidebar --->
+    <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="close-btn" id="close-btn">&times;</a>
+        <a href="home.php">首頁</a>
+        <a href=all.php>所有餐廳</a>
+        <a href=school.php>學餐</a>
+        <a href="backdoor.php">後門</a>
+        <a href="street.php">宵夜街</a>
+        <a href="#">聯絡我們</a>
+    </div>
+
 
 <section class="n-restaurants">
     <div class="center-text">
@@ -126,6 +143,9 @@
         <a href="#" class="btn">查看所有</a>
     </div>
     <!--- custom js link --->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+    crossorigin="anonymous"></script>
     <script src="home.js"></script>
     <script src="myfavfix.js"></script>
 </body>
