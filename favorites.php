5ccdfce1d11ad56352bba8157c964c6462604183
SkyMee12345 <168707310+SkyMee12345@users.noreply.github.com>
2024-05-25 10:01:39 +0800

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root"; 
-$password = "nc@p12rs00a4"; 
+$password = ""; 
 $dbname = "餐廳"; 
 
 $conn1 = mysqli_connect($servername, $username, $password, $dbname);
