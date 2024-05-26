17691ff77e201b9996f1cfdf3eba562f21066d3c
SkyMee12345 <168707310+SkyMee12345@users.noreply.github.com>
2024-05-26 23:07:28 +0800

    Update school.php

--- school.php
+++ school.php
@@ -174,11 +174,6 @@
                     hiddenSubmitButton.click(); // 手動觸發隱藏的提交按鈕
                 }
             });
-
-            const searchBtn = document.querySelector('.search-btn');
-            searchBtn.addEventListener('click', () => {
-                searchForm.submit();
-            });
         });
     </script>
 </body>
