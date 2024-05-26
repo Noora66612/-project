210383af9c2423962e818a057c5c1de58ec0941e
SkyMee12345 <168707310+SkyMee12345@users.noreply.github.com>
2024-05-26 21:19:20 +0800

    Update create_db.php

--- create_db.php
+++ create_db.php
@@ -156,3 +156,23 @@ $servername = "localhost";
               (131, '131.jpg', '全家（九餐）', '校內','320桃園市中壢區中大路300號', '一 ~ 日 7:00-1:00', '無', '034265685')";
 
             $conn->query($sql);
+$servername = "localhost";
+            $username = "root"; 
+            $password = ""; 
+            $dbname2 = "favorites"; 
+            
+            $conn = new mysqli($servername, $username, $password);
+
+            $sql ="CREATE DATABASE IF NOT EXISTS $dbname2";
+            $conn->query($sql);  
+            
+            $conn->select_db($dbname2);
+           $sql = "CREATE TABLE favorites (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                account VARCHAR(255) NOT NULL,
+                restaurant_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                img VARCHAR(255) NOT NULL,
+                hours VARCHAR(255) NOT NULL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )";
